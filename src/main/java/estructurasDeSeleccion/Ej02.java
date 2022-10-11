2313fe7ebc1aede7aa8b10c63b34657176ff9ca7
Guerig Eri <guerig.eri@gmail.com>
2022-10-11 20:13:37 +0200

    subida de local

---
+++ src/main/java/estructurasDeSeleccion/Ej02.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructurasDeSeleccion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ERICK
+ */
+public class Ej02 {
+    public static void main(String[] args) {
+        int x, numero = 135;
+        String resultado;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Escribe un número: ");
+        
+        x = teclado.nextInt();
+        
+        // A
+        //resultado = (x >= numero) ? "Mayor" : "Menor";
+        
+        // B
+        //resultado = (x <= 0) ? "Menor que cero" : "Mayor que cero";
+        
+        
+        
+    }
+}
