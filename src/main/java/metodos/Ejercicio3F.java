d753d782fccb9564fcd1078bb88960e0e421b791
Guerig Eri <guerig.eri@gmail.com>
2022-11-02 16:43:19 +0100

    ejercicio de metodos con la fabrica

---
+++ src/main/java/metodos/Ejercicio3F.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos;
+
+/**
+ *
+ * @author ERICK
+ */
+public class Ejercicio3F {
+
+    /*
+    1   Método mostrarMenuInicial
+    2   Método leerCodigos
+    3   Método filtrarCodigos
+    4   Método leerMateriaPrima
+    5   Método filtrarMateriaPrima
+    6   Método leerManoObra
+    7   Método filtrarManoObra
+    8   Calcular costeProduccion
+    9   Calcular precioUnitario
+    10  Calcular unidadesBeneficio
+    */
+
+
+
+    
+}
