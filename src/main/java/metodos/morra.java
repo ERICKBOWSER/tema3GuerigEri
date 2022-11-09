fb2195e5fd5b87048c886fb039f5c0ebce66551f
Guerig Eri <guerig.eri@gmail.com>
2022-11-09 12:12:56 +0100

    subida morra desde casa

--- src/main/java/metodos/morra.java
+++ src/main/java/metodos/morra.java
@@ -29,9 +29,9 @@ public class morra {
         de 2 victorias sobre el otro jugador: si se da el caso de un empate de 4 a 4, se juega por el punto
         de ventaja, debiendo conseguir otro punto sucesivamente para ganar el partido.        
         */
-        String preguntarJugar, dedos;
+        String preguntarJugar;
         boolean filtrarJugar;
-        int dedosMaquina;
+        int dedosMaquina = 0, sumarDedos, dedos;
         
         do{
             // JUGAR
@@ -41,10 +41,11 @@ public class morra {
                 // PEDIR DEDOS
                 dedos = comprobarNumDedos();
                 
-                if(!dedos.equalsIgnoreCase("salir"))
-                
-                    // DEDOS MÁQUINA
-                    dedosMaquina = dedosMaquina();
+                // DEDOS MÁQUINA
+                dedosMaquina = dedosMaquina();
+
+                // SUMAR DEDOS
+                sumarDedos = sumarDedos(dedos, dedosMaquina);
         
             }else{
                 preguntarJugar = "salir";
@@ -94,16 +95,15 @@ public class morra {
     }
     
     // PREGUNTAR NÚMERO DE DEDOS
-    public static String numeroDedos(){
-        String dedos = "";
+    public static int numeroDedos(){
+        int dedos = 0;
         
         String texto = """
                        Escribe el número de dedos, este tiene que 
                        estar comprendido entre 1 y 5.
                        """;
         
-        dedos = JOptionPane.showInputDialog(texto);
-        dedos = dedos.toLowerCase();
+        dedos = Integer.parseInt(JOptionPane.showInputDialog(texto));
         
         System.out.println(dedos);
         
@@ -111,18 +111,17 @@ public class morra {
     }
     
     // FILTRAR DATOS PERMITIDOS
-    public static boolean filtrarDedos(String respuestaDedos){
-        return(respuestaDedos.equals("1") ||
-                respuestaDedos.equals("2") ||
-                respuestaDedos.equals("3") ||
-                respuestaDedos.equals("4") ||
-                respuestaDedos.equals("5") ||
-                respuestaDedos.equals("salir"));
+    public static boolean filtrarDedos(int respuestaDedos){
+        return(respuestaDedos == 1 || 
+                respuestaDedos == 2 ||
+                respuestaDedos == 3 ||
+                respuestaDedos == 4 ||
+                respuestaDedos == 5);
     }
     
     // COMPROBAR SI EL NÚMERO DE DEDOS ES CORRECTO
-    public static String comprobarNumDedos(){
-        String dedos = "";
+    public static int comprobarNumDedos(){
+        int dedos = 0;
         do{
             dedos = numeroDedos();
         }while(!filtrarDedos(dedos));
@@ -144,7 +143,43 @@ public class morra {
         return maquinaDedos;
     }
     
+    // SUMA JUGADOR
+    public static int resultadoSumaDedos(){
+        int dedos = 0;
+        
+        do{
+            try{
+                dedos = Integer.parseInt(JOptionPane.showInputDialog("Cuanto crees que será la suma de todos los dedos"));          
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null, "El tipo de dato introducido es incorrecto, vuelve a escribirlo.");
+            }
+
+            System.out.println("Suma usuario: " + dedos);
+            
+        }while(dedos <= 1 || dedos > 10);     
+        
+        return dedos;
+    }
     
+    // DEDOS MÁQUINA
+    public static int sumaDedosMaquina(){
+        int maquinaDedos;
+        Random generador = new Random();
+        
+        maquinaDedos = generador.nextInt(2, 10);
+        
+        System.out.println("Maquina suma: " + maquinaDedos);
+        
+        return maquinaDedos;
+    }
+    
+    public static int sumarDedos(int dedos, int dedosMaquina){
+        int sumaTotal = 0;
+        
+        sumaTotal = dedos + dedosMaquina;
+        
+        return sumaTotal;
+    }
     
     
     
